dab5433b7e1078937c36c1333f81e6754dc4d953
Jonathan Hanks <jonathan dot k dot hanks at gmail dot com>
2012-12-15 12:49:55 -0800

    Change from doing a cross product to find an orthoginal vector as the branching point for curve to using a matrix based rotation.  This allows arbitrary branching directions.
    Added a new parameter to the request (pi) which is number of pi radians of rotation to use.  The default is .5 (ie 90 degrees).
    
    I need to rework the positioning logic so that curve is displayed properly.  Currently for some angles it goes off screen.

--- svgfractal.go
+++ svgfractal.go
@@ -10,6 +10,7 @@ package main
 import (
 	"fmt"
 	"github.com/ajstarks/svgo"
+	"math"
 	"net/http"
 	"strconv"
 )
@@ -22,6 +23,44 @@ type Point struct {
 // 2d vector
 type Vector struct {
 	Point
+}
+
+type Matrix struct {
+	Rows [][]float64
+}
+
+func NewMatrix() *Matrix {
+	m := &Matrix{Rows: make([][]float64, 2)}
+	for i := range m.Rows {
+		m.Rows[i] = make([]float64, 2)
+	}
+	m.Identity()
+	//m := Matrix{Rows: {{1.0, 0.0}, {0.0, 1.0}}}
+
+	return m
+}
+
+// Set a matrix to be an identity matrix
+func (m *Matrix) Identity() {
+	for i := range m.Rows {
+		for j := range m.Rows[i] {
+			if i == j {
+				m.Rows[i][j] = 1.0
+			} else {
+				m.Rows[i][j] = 0.0
+			}
+		}
+	}
+}
+
+// rotate around the Z azis by theta degrees
+func (m *Matrix) Rotate(theta float64) {
+	cosT := math.Cos(theta)
+	sinT := math.Sin(theta)
+	m.Rows[0][0] = cosT
+	m.Rows[0][1] = -sinT
+	m.Rows[1][0] = sinT
+	m.Rows[1][1] = cosT
 }
 
 // A 2d line composed of a point, direction
@@ -103,12 +142,28 @@ func cross(v Vector) Vector {
 	return Vector{Point{X: y1*z2 - z1*y2, Y: z1*x1 - x1*z2}}
 }
 
+// Take the product of m * v and store the result in dest
+// This function is provided to reduce the amount of garbage
+// created
+func MultMatrixVectorWithDest(m *Matrix, v, dest *Vector) {
+	dest.X = m.Rows[0][0]*v.X + m.Rows[0][1]*v.Y
+	dest.Y = m.Rows[1][0]*v.X + m.Rows[1][1]*v.Y
+}
+
+// return the product of m * v
+func MultMatrixVector(m *Matrix, v *Vector) *Vector {
+	dest := &Vector{}
+	MultMatrixVectorWithDest(m, v, dest)
+	return dest
+}
+
 // Do the fractal
-func doFractal(s *svg.SVG, l Line, depth int) {
+func doFractal(s *svg.SVG, l Line, depth int, rot *Matrix) {
 	if depth <= 0 {
 		l.Render(s)
 	} else {
-		cdir := cross(l.Direction)
+		//cdir := cross(l.Direction)
+		cdir := MultMatrixVector(rot, &l.Direction)
 		mid := l.At(0.5)
 		l1, l2 := l.Split(0.333333)
 		_, l3 := l2.Split(0.5)
@@ -116,10 +171,10 @@ func doFractal(s *svg.SVG, l Line, depth int) {
 		fmt.Printf("---\n\tl1: %v\n\tl2: %v\n---\n", l1, l3)
 		fmt.Println(cdir)
 		//l1.Render(s)
-		doFractal(s, l1, depth-1)
+		doFractal(s, l1, depth-1, rot)
 		//mid.Render(s)
 
-		lmid := NewLine2(mid, cdir, l1.Scale*1.0)
+		lmid := NewLine2(mid, *cdir, l1.Scale*1.0)
 		//lmid.Render(s)
 
 		mid2 := lmid.At(1.0)
@@ -130,24 +185,29 @@ func doFractal(s *svg.SVG, l Line, depth int) {
 		l2b := NewLine3(mid2, l3.At(0.0))
 		//l2b.Render(s)
 
-		doFractal(s, l2a, depth-1)
-		doFractal(s, l2b, depth-1)
+		doFractal(s, l2a, depth-1, rot)
+		doFractal(s, l2b, depth-1, rot)
 
 		//l3.Render(s)
-		doFractal(s, l3, depth-1)
+		doFractal(s, l3, depth-1, rot)
 	}
 }
 
-func fractalLine(s *svg.SVG, x1, y1, x2, y2, complexity int) {
+func fractalLine(s *svg.SVG, x1, y1, x2, y2, complexity int, rotation float64) {
 	l := NewLine(float64(x1), float64(y1), float64(x2), float64(y2))
 
-	doFractal(s, l, complexity)
+	m := NewMatrix()
+	m.Rotate(rotation)
+
+	doFractal(s, l, complexity, m)
 }
 
 func lineFractal(w http.ResponseWriter, req *http.Request) {
 	const (
 		defaultComplexity = 6
 		maxComplexity     = 9
+
+		defaultPi = 0.5
 	)
 
 	_ = req.ParseForm()
@@ -156,13 +216,25 @@ func lineFractal(w http.ResponseWriter, req *http.Request) {
 		complexity = defaultComplexity
 	}
 
+	pi, err := strconv.ParseFloat(req.FormValue("pi"), 64)
+	if err != nil {
+		pi = defaultPi
+	}
+	if pi == 0.0 || pi == 1.0 {
+		complexity = 0
+	}
+
 	w.Header().Set("Content-Type", "image/svg+xml")
 	s := svg.New(w)
 	width := 1000 + (4000 * complexity / maxComplexity)
 	height := int(float64(width)*0.35) + 50
 	s.Start(width, height)
 	defer s.End()
-	fractalLine(s, 0, height-50, width-1, height-50, complexity)
+	if pi < 0.0 {
+		fractalLine(s, 0, 50, width-1, 50, complexity, -math.Pi*pi)
+	} else {
+		fractalLine(s, 0, height-50, width-1, height-50, complexity, -math.Pi*pi)
+	}
 }
 
 func main() {
